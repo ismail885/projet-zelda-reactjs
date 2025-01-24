0f8798313b1f38ab7fff9495c2c3f678029af4ca
ismail885 <ismail.abouzaid@gmail.com>
2025-01-24 22:30:48 +0100

    fix: correct email input name in contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -33,7 +33,7 @@ const ContactForm = () => {
       <input type="text" name="from_name" required />
 
       <label>Email :</label>
-      <input type="email" name="user_email" required />
+      <input type="email" name="to_name" required />
 
       <label>Message :</label>
       <textarea name="message" required />
