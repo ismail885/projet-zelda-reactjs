6d8f22b17967785fde2741b26e3f5d6d208bacf2
ismail885 <ismail.abouzaid@gmail.com>
2025-01-29 18:46:25 +0100

    fix: reduce excessive line breaks in Footer component

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.scss';
 export default function Footer() {
     return (
     <div>
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <p>&copy; Footer... pied... ok je suis pas drole. &copy;</p>
         <img src="footerzelda.jpg" alt="Un footer tout ce qu'il y a de plus normal" />
     </div>
